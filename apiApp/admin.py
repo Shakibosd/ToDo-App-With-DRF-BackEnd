bac5f5af06888df7e002d92f095b0aec44c96d46
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-09-23 03:03:41 +0600

    finsh

--- apiApp/admin.py
+++ apiApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Plan
+from django.utils.html import format_html
 
 class MyPlanAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'product']
